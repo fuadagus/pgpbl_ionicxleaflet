f7b8268c490c0746f4f55f63c5b81dd8805dbc72
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-09-23 09:59:47 +0700

    memindahkan map dari on init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import * as L from 'leaflet';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  map!: L.Map;
+
 
   constructor() {}
+  ngOnInit() {
+    
+  }
+
+  ionViewDidEnter() {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+
+
 
 }
