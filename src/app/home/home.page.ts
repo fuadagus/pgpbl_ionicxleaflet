fcdbfbad10967dcf878527e180f78bad5e30ef19
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-09-23 10:25:23 +0700

    menambahkan popup dan marker

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,19 +9,51 @@ import * as L from 'leaflet';
 export class HomePage {
   map!: L.Map;
 
-
   constructor() {}
-  ngOnInit() {
-    
-  }
+
+  ngOnInit() {}
 
   ionViewDidEnter() {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    this.map = L.map('map').setView([-7.770115838142679, 110.37794672157405], 15);
+
+    // Define the different base maps
+    const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
-    }).addTo(this.map);
+    });
+
+    const satellite = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenTopoMap contributors'
+    });
+
+    const dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '© CartoDB'
+    });
+
+    // Add the default base to the map
+    osm.addTo(this.map);
+
+    // Define the icon options
+    const icon = L.icon({
+      iconUrl: 'assets/leaflet/marker-icon.png',
+      shadowUrl: 'assets/leaflet/marker-shadow.png',
+      iconSize: [25, 41], // size of the icon
+      iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+      popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
+      shadowSize: [41, 41]  // size of the shadow
+    });
+
+    // Add a marker to the map with the custom icon
+    const marker = L.marker([-7.770115838142679, 110.37794672157405], { icon }).addTo(this.map);
+    marker.bindPopup('UGM broo').openPopup();
+
+    // Define the base maps object
+    const baseMaps = {
+      "OpenStreetMap": osm,
+      "OpenTopoMap": satellite,
+      "Dark": dark
+    };
+
+    // Add the layer control to the map
+    L.control.layers(baseMaps).addTo(this.map);
   }
-
-
-
 }
